8793eb4ab9a555c0abd0c92486647fe1e7369c38
Hailey <hailey@not_an_email.fake>
2025-11-01 14:57:40 -0500

    .

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 sudo systemctl stop bot_mantrachecker.service
 git pull
 sudo systemctl start bot_mantrachecker.service
-journalctl -u bot_mantrachecker.service -b
+journalctl -u bot_mantrachecker.service -b --no-pager
