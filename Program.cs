1d09a3264617c10528405ca6b223b2eecd8885e1
Hailey <hailey@not_an_email.fake>
2025-11-01 14:30:09 -0500

    info

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ var host = builder.Build();
 
 // // Add commands using minimal APIs
 // host.AddSlashCommand("ping", "Ping!", (User usr) => $"Pong! {usr.Username}");
-host.AddSlashCommand("info", "",() =>
+host.AddSlashCommand("info", "Info!",() =>
     $"{State.MessageCorrectionsChecks.Count} uncorrected messages this session\n" +
     $"{State.GoodMantras} good mantras this session");
 // host.AddMessageCommand("Length", (RestMessage message) => message.Content.Length.ToString());
